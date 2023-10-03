5273a2eba002ebd10d22b3b315c9541177370eec
hashbury Env <efefiomarchibong@gmail.com>
2023-10-03 16:34:09 +0100

    recent update

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,13 +1,45 @@
-resource "aws_s3_bucket" "back-end-hash" {
-  bucket = "for-lock-tf"
-  acl    = "private"
+# resource "aws_s3_bucket" "test_purpose" {
+#   bucket = "for-lock-"
+#   acl    = "private"
 
-  versioning {
-    enabled = true
-  }
+#   versioning {
+#     enabled = true
+#   }
+
+#   tags = {
+#     Name        = "for lock tf"
+
+#     Environment = "Dev"
+#   }
+# }
+
+resource "aws_s3_bucket" "test_archibong" {
+  bucket              = "test-archibong"
+  force_destroy       = "true"
+  object_lock_enabled = "false"
+
 
   tags = {
-    Name        = "for lock tf"
+    Name        = "testing"
     Environment = "Dev"
   }
 }
+
+# resource "aws_s3_bucket_versioning" "tf_versioning" {
+#   bucket = "test-hash"
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# # Upload object into the bucket
+# resource "aws_s3_bucket_object" "test_archibong" {
+#   bucket = "test_archibong"
+#   key    = "show up.png"
+#   source = "/home/hashbury/Documents/GitHub/terraform-demo/s3-bucket/another.jpeg"
+
+#   # The filemd5() function is available in Terraform 0.11.12 and later
+#   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+#   # etag = "${md5(file("path/to/file"))}"
+# }
+
--- s3-bucket/provider.tf
+++ s3-bucket/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "ap-southeast-1"
   access_key = "AKIASHGVDRT5WXVM7ZPM"
   secret_key = "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"
 }
