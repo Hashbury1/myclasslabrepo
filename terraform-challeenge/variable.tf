24480d268137b88ae3ee15462af5ffea9821375c
hashbury Env <efefiomarchibong@gmail.com>
2023-10-19 16:39:53 +0100

    Create variable.tf

---
+++ terraform-challeenge/variable.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+    type = string
+    default = "AKIASHGVDRT5WXVM7ZPM"
+    description = "for my access key"
+}
+
+variable "secret_key" {
+    type = string 
+    default = "fzbn7NLPGzOg0Th3t1+RXBbsw1JMm4P9TQXpW1en"
+    description = "for my secret key"  
+}
+
+variable "bucketname" {
+    type = string
+    default = "testing-hash"
+} 
